284e0acaa521c354eb4e93239486dbf43cca7f08
Gianluca Mazza <gianluca.mazza@scalingparrots.com>
2024-06-12 10:28:47 +0200

    wip

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Cleanup data, logs, scalers, models, predictions
+rm -rf data/*
+rm -rf logs/*
+rm -rf models/*
+rm -rf predictions/*
+rm -rf scalers/*
--- run.sh
+++ run.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Cleanup data, logs, scalers, models, predictions
-rm -rf data/*
-rm -rf logs/*
-rm -rf models/*
-rm -rf predictions/*
-rm -rf scalers/*
+# Run cleanup.sh
+bash cleanup.sh
 
 # Prepare the data
 python3 src/data_preparation.py
