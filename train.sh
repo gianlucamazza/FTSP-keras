6ca9f7f60a13b8d1c26669369eda02ef39360a6e
Gianluca Mazza <gianluca.mazza@scalingparrots.com>
2024-06-24 16:34:40 +0200

    wip

--- train.sh
+++ train.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Create a conda environment
-conda create --name myenv python=3.11 -y
-
-# Activate the conda environment
-source "$(conda info --base)/etc/profile.d/conda.sh"
-conda activate myenv
-
-# Configure conda to use the conda-forge channel
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-
-# Install dependencies from conda-forge
-conda install -y pandas numpy tensorflow featuretools statsmodels matplotlib scikit-learn keras joblib yfinance pyqt optuna optuna-integration tensorflow-cloud
-
 # Run cleanup.sh
 bash cleanup.sh
 
