b1b631dcc25db64e30584be08473a2406a46d7f1
Gianluca Mazza <gianluca.mazza@scalingparrots.com>
2024-07-03 12:50:53 +0200

    fix callbacks

--- src/models/callbacks.py
+++ src/models/callbacks.py
@@ -2,7 +2,6 @@ import sys
 import datetime
 from pathlib import Path
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, TensorBoard
-
 from src.logging.logger import setup_logger
 
 # Ensure the project directory is in the sys.path
@@ -18,6 +17,7 @@ def prepare_callbacks(model_dir: Path, ticker: str, monitor: str = 'val_loss', f
     Prepare callbacks for training the model.
 
     Parameters:
+    - model_dir: Directory where the model will be saved.
     - ticker: Ticker symbol for logging and file naming.
     - monitor: Metric to monitor for early stopping and checkpoints.
     - fold_index: Index of the fold for cross-validation.
@@ -29,12 +29,15 @@ def prepare_callbacks(model_dir: Path, ticker: str, monitor: str = 'val_loss', f
     logger.info(f"  - Monitor metric: {monitor}")
     logger.info(f"  - Fold index: {fold_index}")
 
+    # Create a timestamp for the log directory
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     log_dir = ROOT_DIR / 'logs' / ticker / timestamp
     log_dir.mkdir(parents=True, exist_ok=True)
 
-    filepath = ROOT_DIR / "models" / f"{ticker}/{ticker}_fold_{fold_index}_{monitor}.keras"
+    # Define the file path for the model checkpoints
+    filepath = model_dir / f"{ticker}_fold_{fold_index}_{monitor}.keras"
 
+    # List of callbacks
     callbacks = [
         EarlyStopping(monitor=monitor, patience=10, verbose=1, restore_best_weights=True),
         ModelCheckpoint(filepath=str(filepath), monitor=monitor, save_best_only=True, verbose=1),
